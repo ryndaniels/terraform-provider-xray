6c968675423257b86644c1f8fe8b6f71aa68c35f
Ryn Daniels <ryn@hashicorp.com>
2020-11-02 16:05:18 +0100

    another watch test, add a readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# Terraform Provider Xray
+
+## Build the Provider
+If you're building the provider, follow the instructions to [install it as a plugin](https://www.terraform.io/docs/plugins/basics.html#installing-a-plugin).
+After placing it into your plugins directory,  run `terraform init` to initialize it.
+
+Requirements:
+- [Terraform](https://www.terraform.io/downloads.html) 0.11
+- [Go](https://golang.org/doc/install) 1.13+ (to build the provider plugin)
+
+Clone repository to: `$GOPATH/src/github.com/ryndaniels/terraform-provider-xray
+
+Enter the provider directory and build the provider
+
+```sh
+cd $GOPATH/src/github.com/ryndaniels/terraform-provider-xray
+go build
+```
+
+To install the provider
+```sh
+cd $GOPATH/src/github.com/ryndaniels/terraform-provider-xray
+go install
+```
+
+## Contributors
+This is a best effort provider at the moment. Pull requests, issues and comments are welcomed.
--- pkg/jfrogxray/resource_xray_watch_test.go
+++ pkg/jfrogxray/resource_xray_watch_test.go
@@ -22,16 +22,61 @@ func TestAccWatch_basic(t *testing.T) {
 		Providers:    testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccXrayWatch_Basic(watchName, watchDesc),
+				Config: testAccXrayWatch_basic(watchName, watchDesc),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", watchName),
 					resource.TestCheckResourceAttr(resourceName, "description", watchDesc),
+					resource.TestCheckResourceAttr(resourceName, "resources.0.bin_mgr_id", "123456"),
+					resource.TestCheckResourceAttr(resourceName, "assigned_policies.0.name", "policy_name"),
+					resource.TestCheckResourceAttr(resourceName, "assigned_policies.0.type", "security"),
 				),
 			},
 			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccWatch_filters(t *testing.T) {
+	watchName := "test watch"
+	watchDesc := "watch created by xray acceptance tests"
+	filterValue := "Debian"
+	updatedDesc := "updated watch description"
+	updatedValue := "Docker"
+	resourceName := "xray_watch.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckWatchDestroy(resourceName),
+		Providers:    testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccXrayWatch_filters(watchName, watchDesc, filterValue),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", watchName),
+					resource.TestCheckResourceAttr(resourceName, "description", watchDesc),
+					resource.TestCheckResourceAttr(resourceName, "resources.0.filters.0.type", "package-type"),
+					resource.TestCheckResourceAttr(resourceName, "resources.0.filters.0.value", filterValue),
+					resource.TestCheckResourceAttr(resourceName, "resources.0.type", "repository"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccXrayWatch_filters(watchName, updatedDesc, updatedValue),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", watchName),
+					resource.TestCheckResourceAttr(resourceName, "description", updatedDesc),
+					resource.TestCheckResourceAttr(resourceName, "resources.0.filters.0.type", "package-type"),
+					resource.TestCheckResourceAttr(resourceName, "resources.0.filters.0.value", updatedValue),
+					resource.TestCheckResourceAttr(resourceName, "resources.0.type", "repository"),
+				),
 			},
 		},
 	})
@@ -58,7 +103,7 @@ func testAccCheckWatchDestroy(id string) func(*terraform.State) error {
 	}
 }
 
-func testAccXrayWatch_Basic(name, description string) string {
+func testAccXrayWatch_basic(name, description string) string {
 	return fmt.Sprintf(`
 	resource "xray_watch" "test" {
 		name  = "%s"
@@ -72,9 +117,35 @@ func testAccXrayWatch_Basic(name, description string) string {
 		assigned_policies [
 			{
 				name = "policy_name"
-				type = "security
+				type = "security"
 			}
 		]
 	}
 `, name, description)
+}
+
+func testAccXrayWatch_filters(name, description, filterValue string) string {
+	return fmt.Sprintf(`
+	resource "xray_watch" "test" {
+		name  = "%s"
+		description = "%s"
+		resources [
+			{
+				type = "repository"
+				filters [
+					{
+						type = "package-type"
+						value = "%s"
+					}
+				]
+			}
+		]
+		assigned_policies [
+			{
+				name = "policy_name"
+				type = "security"
+			}
+		]
+	}
+`, name, description, filterValue)
 }
